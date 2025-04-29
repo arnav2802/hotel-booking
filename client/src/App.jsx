2132a802dae2e82d4fdd24d75f36491d0b8c049f
arnav2802 <arnav171103@gmail.com>
2025-04-29 11:57:36 +0530

    client side

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Navbar from "./components/Navbar.jsx";
+import {Route, Routes, useLocation} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Footer from "./components/Footer.jsx";
+import AllRooms from "./pages/AllRooms.jsx";
+import RoomDetails from "./pages/RoomDetails.jsx";
+
+const App = () => {
+
+    const isOwnerPath = useLocation().pathname.includes()
+
+    return (
+        <div>
+            { !isOwnerPath && <Navbar/> }
+            <div className="min-h-[70vh]">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/rooms" element={<AllRooms/>}/>
+                    <Route path="/rooms/:id" element={<RoomDetails/>}/>
+                </Routes>
+            </div>
+            <Footer/>
+        </div>
+    )
+}
+export default App
